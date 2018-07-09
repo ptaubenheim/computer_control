1ced7aff57ab12d816631d25fa6482116cdd525e
Patrick Taubenheim <taubenheim@patrick-taubenheim.de>
2018-07-09 10:48:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+import pygame
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+liste = ["a", "b", "c", "d", "e"]
+
+for x in liste:
+    print(x)
+
+for x in range(len(liste)):
+    print(liste[x])
+
+var = "blub"
+var2 = 5
+var3 = var + str(var2)
+print(var3)
